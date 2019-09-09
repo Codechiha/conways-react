44ea34ade0382d1b33c270a728e504b90f3762d5
David Situ <davidsitu626@gmail.com>
2019-09-09 09:41:07 -0700

    Main, Grid, Box, Buttons Components added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ButtonToolbar, Dropdown, DropdownButton } from 'react-bootstrap';
 
 //1. Create the Main Component that will Contain everything
 //1a. Everything is Title, Grid, Generation count
@@ -29,20 +30,20 @@ class Box extends React.Component {
 
 class Grid extends React.Component {
     render() {
-        const width = this.props.cols * 14;
-        let rowsArr = []
+        const width = (this.props.cols * 14);
+        var rowsArr = []
 
-        let boxClass = "";
+        var boxClass = "";
         //For every coordinate of rows and cols
         //Create a box ID
         //use CSS for box on and box off
         //push a Box Component per coordinate
         //The Box component will have properties and selected function
-        for (let i = 0; i < this.props.rows; i++) {
-            for (let j = 0; j < this.props.rows; j++) {
+        for (var i = 0; i < this.props.rows; i++) {
+            for (var j = 0; j < this.props.cols; j++) {
                 let boxId = i + "_" + j;
 
-                boxClass = this.props.gridFull[i][j] ? "box on" : "box off"
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
                 rowsArr.push(
                     <Box 
                         boxClass = {boxClass}
@@ -60,6 +61,45 @@ class Grid extends React.Component {
                 {rowsArr}
             </div>
         );
+    }
+}
+
+class Buttons extends React.Component {
+    
+    render() {
+        return (
+            <div className="center">
+                <ButtonToolbar>
+                    <button className="btn btn-default" onClick={this.props.playButton}>
+                        Play
+                    </button>
+                    <button className="btn btn-default" onClick={this.props.pauseButton}>
+                        Pause
+                    </button>
+                    <button className="btn btn-default" onClick={this.props.clear}>
+                        Clear
+                    </button>
+                    <button className="btn btn-default" onClick={this.props.slow}>
+                        Slow
+                    </button>
+                    <button className="btn btn-default" onClick={this.props.fast}>
+                        Fast
+                    </button>
+                    <button className="btn btn-default" onClick={this.props.seed}>
+                        Seed
+                    </button>
+                    <DropdownButton
+                        title="Grid Size"
+                        id="size-menu"
+                        onSelect={this.handleSelect}
+                    >
+                        <Dropdown.Item eventKey="1">20x10</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">50x30</Dropdown.Item>
+                        <Dropdown.Item eventKey="3">70x10</Dropdown.Item>
+                    </DropdownButton>     
+                </ButtonToolbar>
+            </div>
+        )
     }
 }
 
@@ -102,25 +142,65 @@ class Main extends React.Component {
 
     playButton = () => {
         clearInterval(this.intervalId)
-        this.intervalId = setInterval(this.playButton, this.speed);
+        this.intervalId = setInterval(this.play, this.speed);
+    }
+
+    pauseButton = () => {
+        clearInterval(this.intervalId);
     }
 
     play = () => {
         let g = this.state.gridFull;
         let g2 = arrayClone(this.state.gridFull);
-        
+        //Rules algorithms
+        //  1. Births: Each dead cell adjacent to exactly three live neighbors will become live in the next generation.
+        //  2. Death by isolation: Each live cell with one or fewer live neighbors will die in the next generation.
+        //  3. Death by overcrowding: Each live cell with four or more live neighbors will die in the next generation.
+        //  4. Survival: Each live cell with either two or three live neighbors will remain alive for the next generation.
+        for(let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.cols; j++) {
+                //Counting Neighbor
+                let count = 0;
+                if (i > 0) if (g[i-1][j]) count++;
+                if (i > 0 && j > 0) if (g[i-1][j - 1]) count++;
+                if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+                if (j < this.cols - 1) if (g[i][j+1]) count++;
+                if (j > 0) if (g[i][j - 1]) count++;
+                if (i < this.rows - 1) if (g[i + 1][j]) count++;
+                if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+                if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+                // g[i][j] is a live iteration of the grid
+                if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+                // !g[i][j] is a dead iteration of the grid
+                if (!g[i][j] && count === 3) g2[i][j] = true;
+            }
+        }
+        this.setState({
+            gridFull: g2,
+            generation: this.state.generation + 1
+        });
     }
 
     //Lifecycle Hook: componentDidMount(){}
     //Loads once component runs
     componentDidMount() {
         this.seed();
+        this.playButton();
     }
 
     render() {
         return (
             <div>
                 <h1>The Game of Life</h1>
+                <Buttons 
+                    playButton={this.playButton}
+                    pauseButton={this.pauseButton}
+                    slow={this.slow}
+                    fast={this.fast}
+                    clear={this.clear}
+                    seed={this.seed}
+                    gridSize={this.gridSize}
+                />
                 <Grid 
                     gridFull = {this.state.gridFull}
                     rows = {this.rows}
